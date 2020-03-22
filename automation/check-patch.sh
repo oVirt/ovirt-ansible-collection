92e2bdf32cccad4f23e8ec4e43b47ebc046c1896
mnecas <necas.marty@gmail.com>
2020-03-22 11:34:28 +0100

    Add ansible-sanity tests

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -25,3 +25,9 @@ rpmbuild \
 [[ -d exported-artifacts ]] || mkdir -p exported-artifacts
 find output -iname \*rpm -exec mv "{}" exported-artifacts/ \;
 mv *.tar.gz exported-artifacts
+
+yum install $PWD/output/*.rpm
+cd "/usr/share/ansible/collections/ansible_collections/ovirt/ovirt_collection"
+ansible-test sanity --test validate-modules
+ansible-test sanity --test pep8
+ansible-test sanity --test pylint
