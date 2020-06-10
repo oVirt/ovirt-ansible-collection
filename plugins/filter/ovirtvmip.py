f5c7cd6df6ea780a52fda08fd7c629e16339b0d7
Martin Nečas <necas.marty@gmail.com>
2020-06-10 18:22:10 +0200

    Add ovirtdiff filter and filter examples (#37)
    * Add compare resource filter
    
    * add vmips examples
    
    * add example for specific network
    
    * add blank line to vmips
    
    * fix pep8 issue

--- plugins/filter/ovirtvmip.py
+++ plugins/filter/ovirtvmip.py
@@ -16,6 +16,31 @@ class FilterModule(object):
             'ovirtvmipsv6': self.ovirtvmipsv6,
             'filtervalue': self.filtervalue,
             'removesensitivevmdata': self.removesensitivevmdata,
+            'ovirtdiff': self.ovirtdiff,
+        }
+
+    def ovirtdiff(self, vm1, vm2):
+        """
+        This filter takes two dictionaries of two different resources and compare
+        them. It return dictionari with keys 'before' and 'after', where 'before'
+        containes old values of resources and 'after' contains new values.
+        This is mainly good to compare current VM object and next run VM object to see
+        the difference for the next_run.
+        """
+        before = []
+        after = []
+        if vm1.get('next_run_configuration_exists'):
+            keys = [
+                key for key in set(list(vm1.keys()) + list(vm2.keys()))
+                if (key in vm1 and (key not in vm2 or vm2[key] != vm1[key])) or (key in vm2 and (key not in vm1 or vm1[key] != vm2[key]))
+            ]
+            for key in keys:
+                before.append((key, vm1.get(key)))
+                after.append((key, vm2.get(key, vm1.get(key))))
+
+        return {
+            'before': dict(before),
+            'after': dict(after),
         }
 
     def filtervalue(self, data, attr, value):
