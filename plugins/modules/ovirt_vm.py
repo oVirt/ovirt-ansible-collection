7286567fa12fa11a56d5022b4eda4b84c5ab5487
mnecas <necas.marty@gmail.com>
2020-07-23 10:27:53 +0200

    fix order of exceptions

--- plugins/modules/ovirt_vm.py
+++ plugins/modules/ovirt_vm.py
@@ -1673,11 +1673,11 @@ class VmsModule(BaseModule):
         sds_service = self._connection.system_service().storage_domains_service()
         sds = sds_service.list(search='name="{}"'.format(self.param('storage_domain') if self.param('storage_domain') else "*"))
         disks = self.__get_cds_from_sds(sds)
-        if disks and len(disks) > 1:
-            raise ValueError('Found mutiple disks with same name "{}" please use \
-                disk ID in "cd_iso" to specify which disk should be used.'.format(self.param('cd_iso')))
         if not disks:
             raise ValueError('Was not able to find disk with name or id "{}".'.format(self.param('cd_iso')))
+        if len(disks) > 1:
+            raise ValueError('Found mutiple disks with same name "{}" please use \
+                disk ID in "cd_iso" to specify which disk should be used.'.format(self.param('cd_iso')))
         return disks[0].id
 
     def _attach_cd(self, entity):
