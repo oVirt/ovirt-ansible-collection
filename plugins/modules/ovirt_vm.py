c1d9482d9295be937d33519c8588a65c63f3b017
mnecas <necas.marty@gmail.com>
2020-07-23 10:04:50 +0200

    fix disk search

--- plugins/modules/ovirt_vm.py
+++ plugins/modules/ovirt_vm.py
@@ -1662,7 +1662,9 @@ class VmsModule(BaseModule):
 
     def __get_cds_from_sds(self, sds):
         for sd in sds:
-            disks = list(filter(lambda x: x.name == self.param('cd_iso') and x.content_type == otypes.DiskContentType.ISO,
+            if sd.disks is None:
+                continue
+            disks = list(filter(lambda x: x.name == self.param('cd_iso') or x.id == self.param('cd_iso') and x.content_type == otypes.DiskContentType.ISO,
                                 self._connection.follow_link(sd.disks)))
             if disks:
                 return disks
@@ -1671,12 +1673,11 @@ class VmsModule(BaseModule):
         sds_service = self._connection.system_service().storage_domains_service()
         sds = sds_service.list(search='name="{}"'.format(self.param('storage_domain') if self.param('storage_domain') else "*"))
         disks = self.__get_cds_from_sds(sds)
-        if len(disks) > 1:
+        if disks and len(disks) > 1:
             raise ValueError('Found mutiple disks with same name "{}" please use \
                 disk ID in "cd_iso" to specify which disk should be used.'.format(self.param('cd_iso')))
         if not disks:
-            # The `cd_iso` is valid disk ID returning to _attach_cd
-            return disks_service.disk_service(self.param('cd_iso')).get().id
+            raise ValueError('Was not able to find disk with name or id "{}".'.format(self.param('cd_iso')))
         return disks[0].id
 
     def _attach_cd(self, entity):
