0b1e85065c220271db9bf2cc66618d7656dac7d2
Martin Nečas <necas.marty@gmail.com>
2020-09-16 09:22:46 +0200

    Release 1.2.0-0.1 (#121)
    * Release 1.2.0-0.1
    
    * remove milestone

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-VERSION="1.1.3"
-MILESTONE=master
-RPM_RELEASE="0.1.$MILESTONE.$(date -u +%Y%m%d%H%M%S)"
+VERSION="1.2.0"
+MILESTONE=
+RPM_RELEASE="0.1"
 
 BUILD_TYPE=$2
 BUILD_PATH=$3
