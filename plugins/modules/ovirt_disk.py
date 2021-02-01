2cb951a1c36ec866c2c2d04e82868dbf91bd8655
mnecas <necas.marty@gmail.com>
2021-02-01 17:52:24 +0100

    revert ovirt_disk change

--- plugins/modules/ovirt_disk.py
+++ plugins/modules/ovirt_disk.py
@@ -457,14 +457,6 @@ def transfer(connection, module, direction, transfer_func):
     finally:
         transfer_service.finalize()
 
-        disks_service = connection.system_service().disks_service()
-        wait(
-            service=disks_service.service(module.params['id']),
-            condition=lambda d: d.status == otypes.DiskStatus.OK,
-            wait=module.params['wait'],
-            timeout=module.params['timeout'],
-        )
-
         while transfer.phase in [
             otypes.ImageTransferPhase.TRANSFERRING,
             otypes.ImageTransferPhase.FINALIZING_SUCCESS,
