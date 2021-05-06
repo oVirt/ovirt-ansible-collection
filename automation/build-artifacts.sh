341cae860e57508b59cac6a9ba8e7575dc5979b1
mnecas <necas.marty@gmail.com>
2021-05-06 16:08:06 +0200

    run diff myhead

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -17,8 +17,10 @@ rm -f ./*tar.gz
 git remote -v
 git fetch origin master
 echo $(git branch -a)
+echo $(git diff myhead)
+
 # If PR changed something in ./plugins or ./roles it is required to have changelog
-if [[ $(git diff --quiet origin/master ./plugins ./roles)$? -eq 1 && $(git diff --quiet origin/master ./changelogs)$? -eq 0 ]]; then
+if [[ $(git diff --quiet myhead ./plugins ./roles)$? -eq 1 && $(git diff --quiet myhead ./changelogs)$? -eq 0 ]]; then
         echo "Please add changelog.";
         exit 1;
 fi
