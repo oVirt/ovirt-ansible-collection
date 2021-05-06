e7eb5ca2b3057e584cf36f303acbdc9f0a21edbb
mnecas <necas.marty@gmail.com>
2021-05-06 11:25:12 +0200

    add debug br

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -15,7 +15,7 @@ rm -f ./*tar.gz
 ./build.sh build rhv $ROOT_PATH
 
 git remote -v
-
+echo $(git branch -a)
 # If PR changed something in ./plugins or ./roles it is required to have changelog
 if [[ $(git diff --quiet origin ./plugins ./roles)$? -eq 1 && $(git diff --quiet origin ./changelogs)$? -eq 0 ]]; then
         echo "Please add changelog.";
