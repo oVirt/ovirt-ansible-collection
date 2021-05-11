787efe9192f97752c892b43834b1492d8c71c894
Martin Nečas <necas.marty@gmail.com>
2021-05-11 14:58:43 +0200

    automation: add changelog check (#268)
    * automation: add changelog check
    
    * add changelog
    
    * remove change and changelog

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -75,3 +75,9 @@ ansible-test sanity
 /usr/local/bin/ansible-lint roles/* -x 204
 
 cd $ROOT_PATH
+
+# If PR changed something in ./plugins or ./roles it is required to have changelog
+if [[ $(git diff --quiet HEAD^ ./plugins ./roles)$? -eq 1 && $(git diff --quiet HEAD^ ./changelogs)$? -eq 0 ]]; then
+        echo "Please add changelog.";
+        exit 1;
+fi
