2909a78187ce134b00ebc0bc0a61e291be9092d9
mnecas <necas.marty@gmail.com>
2021-09-20 14:53:44 +0200

    fix pep8

--- plugins/modules/ovirt_disk.py
+++ plugins/modules/ovirt_disk.py
@@ -907,9 +907,7 @@ def main():
                 module.params.get('activate'),
                 module.params.get('bootable'),
                 module.params.get('uses_scsi_reservation'),
-                module.params.get('pass_discard'),
-                ]
-            ):
+                module.params.get('pass_discard'), ]):
             module.warn("Cannot use 'interface', 'activate', 'bootable', 'uses_scsi_reservation' or 'pass_discard' without specifing VM.")
 
         # When the host parameter is specified and the disk is not being
