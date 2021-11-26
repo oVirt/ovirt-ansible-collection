9cc2b86da4ec7af06db7c8951b73c4ef0d961569
Martin Nečas <necas.marty@gmail.com>
2021-11-26 15:59:58 +0100

    backport info: bump deprecate version for fetch_nested and nested_attribute (#385)
    * info: bump deprecate version for fetch_nested and nested_attributes (#378)
    
    * info: bump deprecate version for fetch_nested and nested_attributes params
    
    * add changelog
    
    * Remove rebase comments
    
    * Remove typo
    
    * Fix ovirt_cluster_info version

--- plugins/modules/ovirt_vm_info.py
+++ plugins/modules/ovirt_vm_info.py
@@ -136,8 +136,8 @@ def main():
     check_sdk(module)
     if module.params['fetch_nested'] or module.params['nested_attributes']:
         module.deprecate(
-            "The 'fetch_nested' and 'nested_attributes' are deprecated please use 'follows' parameter",
-            version='2.0.0',
+            "The 'fetch_nested' and 'nested_attributes' are deprecated please use 'follow' parameter",
+            version='3.0.0',
             collection_name='ovirt.ovirt'
         )
 
