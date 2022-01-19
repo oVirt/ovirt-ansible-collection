2cede5dcd8f305024b502d41df0f776035d59807
mnecas <necas.marty@gmail.com>
2022-01-19 20:38:32 +0100

    add api info

--- plugins/modules/ovirt_api_info.py
+++ plugins/modules/ovirt_api_info.py
@@ -17,6 +17,15 @@ description:
     - "Retrieve information about the oVirt/RHV API."
     - This module was called C(ovirt_api_facts) before Ansible 2.9, returning C(ansible_facts).
       Note that the M(@NAMESPACE@.@NAME@.ovirt_api_info) module no longer returns C(ansible_facts)!
+    follow:
+      description:
+        - List of linked entities, which should be fetched along with the main entity.
+        - This parameter replaces usage of C(fetch_nested) and C(nested_attributes).
+        - "All follow parameters can be found at following url: https://ovirt.github.io/ovirt-engine-api-model/master/#types/api/links_summary"
+      type: list
+      version_added: 1.5.0
+      elements: str
+      aliases: ['follows']
 notes:
     - "This module returns a variable C(ovirt_api),
        which contains a information about oVirt/RHV API. You need to register the result with
