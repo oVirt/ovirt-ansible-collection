9c26c9dc2850108bdece7b3671690c85bbcabb62
Martin Nečas <necas.marty@gmail.com>
2022-04-11 11:54:44 +0200

    Fix hosts

--- plugins/modules/ovirt_affinity_group.py
+++ plugins/modules/ovirt_affinity_group.py
@@ -178,7 +178,7 @@ class AffinityGroupsModule(BaseModule):
         to_remove = list(vm for vm in assigned_vms if vm not in self._vm_ids)
         to_add = []
         if self._vm_ids:
-            to_add = list(vm for vm in self._vm_ids and vm not in assigned_vms)
+            to_add = list(vm for vm in self._vm_ids if vm not in assigned_vms)
         ag_service = self._service.group_service(affinity_group.id)
         for vm in to_remove:
             ag_service.vms_service().vm_service(vm).remove()
@@ -237,7 +237,7 @@ class AffinityGroupsModule(BaseModule):
         ) else None
 
         affinity_group.hosts = [
-            otypes.AffinityLabel(id=host_id) for host_id in self._host_ids
+            otypes.Host(id=host_id) for host_id in self._host_ids
         ] if self._host_ids is not None else None
         affinity_group.vm_labels = [
             otypes.AffinityLabel(id=host_id) for host_id in self._vm_label_ids
