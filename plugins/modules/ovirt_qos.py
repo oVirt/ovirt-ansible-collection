68dff9672bcd7637b3fe23954f5d73ee40ccd24f
Martin Nečas <necas.marty@gmail.com>
2022-06-14 08:48:02 +0200

    Add cpu, host, vm params

--- plugins/modules/ovirt_qos.py
+++ plugins/modules/ovirt_qos.py
@@ -64,6 +64,67 @@ options:
             - "The max number of read throughput. If passed you can't pass a value for C(max_throughput)"
             - "If no value is given it will default to the HE value, assuming C(max_throughput) hasn't been set"
         type: int
+    cpu_limit:
+        description:
+            - "The maximum processing capability in %."
+            - "Used to configure computing resources."
+        type: int
+    inbound_average:
+        description:
+            - "The desired average inbound bit rate in Mbps (Megabits per sec)."
+            - "Used to configure virtual machines networks. If defined, `inbound_peak` and `inbound_burst` also has to be set."
+            - "See link:https://libvirt.org/formatnetwork.html#elementQoS[Libvirt-QOS] for further details."
+        type: int
+    inbound_peak:
+        description:
+            - "The maximum inbound rate in Mbps (Megabits per sec)."
+            - "Used to configure virtual machines networks. If defined, `inbound_average` and `inbound_burst` also has to be set."
+            - "See link:https://libvirt.org/formatnetwork.html#elementQoS[Libvirt-QOS] for further details."
+        type: int
+    inbound_burst:
+        description:
+            - "The amount of data that can be delivered in a single burst, in MB."
+            - "Used to configure virtual machine networks. If defined, `inbound_average` and `inbound_peak` must also be set."
+            - "See link:https://libvirt.org/formatnetwork.html#elementQoS[Libvirt-QOS] for further details."
+        type: int
+    outbound_average:
+        description:
+            - "The desired average outbound bit rate in Mbps (Megabits per sec)."
+            - "Used to configure virtual machines networks. If defined, `outbound_peak` and `outbound_burst` also has to be set."
+            - "See link:https://libvirt.org/formatnetwork.html#elementQoS[Libvirt-QOS] for further details."
+        type: int
+    outbound_peak:
+        description:
+            - "The maximum outbound rate in Mbps (Megabits per sec)."
+            - "Used to configure virtual machines networks. If defined, `outbound_average` and `outbound_burst` also has to be set."
+            - "See link:https://libvirt.org/formatnetwork.html#elementQoS[Libvirt-QOS] for further details."
+        type: int
+    outbound_burst:
+        description:
+            - "The amount of data that can be sent in a single burst, in MB."
+            - "Used to configure virtual machine networks. If defined, `outbound_average` and `outbound_peak` must also be set."
+            - "See link:https://libvirt.org/formatnetwork.html#elementQoS[Libvirt-QOS] for further details."
+        type: int
+    outbound_average_linkshare:
+        description:
+            - "Weighted share."
+            - "Used to configure host networks. Signifies how much of the logical link's capacity a specific network should be
+              allocated, relative to the other networks attached to the same logical link. The exact share depends on the sum
+              of shares of all networks on that link. By default this is a number in the range 1-100."
+        type: int
+    outbound_average_upperlimit:
+        description:
+            - "The maximum bandwidth to be used by a network in Mbps (Megabits per sec)."
+            - "Used to configure host networks. If `outboundAverageUpperlimit` and
+              `outbound_average_realtime` are provided, the `outbound_averageUpperlimit` must not be lower than the `outbound_average_realtime`."
+        type: int
+    outbound_average_realtime:
+        description:
+            - "The committed rate in Mbps (Megabits per sec)."
+            - "Used to configure host networks. The minimum bandwidth required by a network. The committed rate requested is not
+               guaranteed and will vary depending on the network infrastructure and the committed rate requested by other
+               networks on the same logical link."
+        type: int
     type:
         description:
             - "The type of QoS. Allows for one of storage/cpu/network/hostnetwork"
@@ -177,16 +238,25 @@ class QosModule(BaseModule):
         :return: otypes.QoS
         """
         return otypes.Qos(
-            name=self._module.params.get('name') if self._module.params.get('name') else None,
-            id=self._module.params.get('id') if self._module.params.get('id') else None,
-            type=self._get_qos_type(self._module.params.get('type')),
-            description=self._module.params.get('description') if self._module.params.get('description') else None,
-            max_iops=self._module.params.get('max_iops') if self._module.params.get('max_iops') is not None else None,
-            max_read_iops=self._module.params.get('read_iops') if self._module.params.get('read_iops') is not None else None,
-            max_read_throughput=self._module.params.get('read_throughput') if self._module.params.get('read_throughput') is not None else None,
-            max_throughput=self._module.params.get('max_throughput') if self._module.params.get('max_throughput') is not None else None,
-            max_write_iops=self._module.params.get('write_iops') if self._module.params.get('write_iops') is not None else None,
-            max_write_throughput=self._module.params.get('write_throughput') if self._module.params.get('write_throughput') is not None else None,
+            name=self.param('name'),
+            id=self.param('id'),
+            type=self._get_qos_type(self.param('type')),
+            description=self.param('description'),
+            max_iops=self.param('max_iops'),
+            max_read_iops=self.param('read_iops'),
+            max_read_throughput=self.param('read_throughput'),
+            max_throughput=self.param('max_throughput'),
+            max_write_iops=self.param('write_iops'),
+            cpu_limit=self.param('cpu_limit'),
+            inbound_average=self.param('inbound_average'),
+            inbound_peak=self.param('inbound_peak'),
+            inbound_burst=self.param('inbound_burst'),
+            outbound_average=self.param('outbound_average'),
+            outbound_peak=self.param('outbound_peak'),
+            outbound_burst=self.param('outbound_burst'),
+            outbound_average_linkshare=self.param('outbound_average_linkshare'),
+            outbound_average_upperlimit=self.param('outbound_average_upperlimit'),
+            outbound_average_realtime=self.param('outbound_average_realtime'),
         )
 
 
@@ -216,6 +286,16 @@ def main():
         max_throughput=dict(default=None, type='int'),
         read_throughput=dict(default=None, type='int'),
         write_throughput=dict(default=None, type='int'),
+        cpu_limit=dict(default=None, type='int'),
+        inbound_average=dict(default=None, type='int'),
+        inbound_peak=dict(default=None, type='int'),
+        inbound_burst=dict(default=None, type='int'),
+        outbound_average=dict(default=None, type='int'),
+        outbound_peak=dict(default=None, type='int'),
+        outbound_burst=dict(default=None, type='int'),
+        outbound_average_linkshare=dict(default=None, type='int'),
+        outbound_average_upperlimit=dict(default=None, type='int'),
+        outbound_average_realtime=dict(default=None, type='int'),
         type=dict(default=None)
     )
     module = AnsibleModule(
