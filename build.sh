d479d9cc7ed2d7f0358a0336cf20cbbfa554075c
Martin Nečas <necas.marty@gmail.com>
2022-10-13 10:45:34 +0200

    Release 2.3.0 (#604)

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-VERSION="2.2.4"
-MILESTONE="master"
-RPM_RELEASE="0.1.$MILESTONE.$(date -u +%Y%m%d%H%M%S)"
+VERSION="2.3.0"
+MILESTONE=""
+RPM_RELEASE="1"
 
 BUILD_TYPE=$2
 BUILD_PATH=$3
