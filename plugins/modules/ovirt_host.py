oVirt/ovirt-ansible-collection

commit 9890ad95a0ed5bbeafdfe3ec523054e0f652c750

cluster_upgrade: skip host upgrades without anything to update (#580)
* cluster_upgrade: skip host upgrades without anything to update

* Add changelog

* check if update_available

plugins/modules/ovirt_host.py
@@ -613,7 +613,7 @@ def main():
             ret = hosts_module.action(
                 action='upgrade',
                 action_condition=lambda h: h.update_available,
-                wait_condition=lambda h: h.status == result_state and (
+                wait_condition=lambda h: not h.update_available or h.status == result_state and (
                     len([
                         event
                         for event in events_service.list(

commit 8efe974820c3cde944ab7fa712ea1f540df0ea65

Wait for host to be in result state
Bug introduced in #580.
The update_available is false just after the upgrade, and
HOST_UPGRADE_FINISHED_AND_WILL_BE_REBOOTED is logged. But the host is
not yet rebooted, so we still need to wait for the status to be UP or
MAINTENANCE.

Signed-off-by: Martin Nečas <necas.marty@gmail.com>

plugins/modules/ovirt_host.py
@@ -613,7 +613,7 @@ def main():
             ret = hosts_module.action(
                 action='upgrade',
                 action_condition=lambda h: h.update_available,
-                wait_condition=lambda h: not h.update_available or h.status == result_state and (
+                wait_condition=lambda h: not h.update_available and h.status == result_state and (
                     len([
                         event
                         for event in events_service.list(